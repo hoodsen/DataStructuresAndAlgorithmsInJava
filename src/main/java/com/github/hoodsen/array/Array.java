97ad843e575be317d001782c14a007e9e0203770
Jason Robison <hoodsen@gmail.com>
2015-04-02 21:02:19 -0600

    Added skeleton Array and ArrayTest classes

---
+++ src/main/java/com/github/hoodsen/array/Array.java
@@ -0,0 +1,5 @@
+package com.github.hoodsen.array;
+
+public class Array {
+
+}
---
+++ src/test/java/com/github/hoodsen/array/ArrayTest.java
@@ -0,0 +1,14 @@
+package com.github.hoodsen.array;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
